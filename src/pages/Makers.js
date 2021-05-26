a460f5938dbbdd4e40b63c8e00b4a29d5b473b54
Laura <laura.jarventie@edu.bc.fi>
2021-05-26 15:36:20 +0300

    artesaans page box

--- src/pages/Makers.js
+++ src/pages/Makers.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/Card";
 
 const Makers = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Card style={{ width: "50rem" }}>
+        <Card.Body>
+          <Card.Title>Riitta Järventie</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Tuotteet: kassit, pussukat, käsilaukut
+          </Card.Subtitle>
+          <Card.Text>
+            Olen aina pitänyt käsitöiden tekemisestä ja nyt eläkeläisenä minulla
+            on runsaasti aikaa tehdä erilaisia tuotteita. Ohessa olevasta
+            linkistä löydät lisää tuotoksiani.
+          </Card.Text>
+          <Card.Link href="https://pussukat.wordpress.com/">
+            Pussukat kotisivu
+          </Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Makers;
