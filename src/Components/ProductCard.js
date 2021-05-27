836ce6bd8cb07b10eb379d9ec645c96256016dcf
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-05-27 10:41:04 +0300

    Product card to bootstrap change

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 
-const ProductCard = ({id, nimi, kuva, tekijä, hinta, kategoria}) => {
+const ProductCard = ({ id, nimi, kuva, tekijä, hinta, kategoria }) => {
   return (
     <div className="productCard">
-      <Link to={`/tuotteet/${id}`}>
-        <p>{nimi}</p>
-      </Link>
-      <Link to={`/tuotteet/${id}`}>
-        <img src={kuva[0].kuva} alt="tuotteen kuva" />
-      </Link>
-      <p>Artisaani: {tekijä}</p>
-      <p>Hinta: {hinta}</p>
-      <p>Kategoria: {kategoria}</p>
+      <Card style={{ width: "20rem" }}>
+        <Card.Title>
+          {" "}
+          <Link to={`/tuotteet/${id}`}>
+            <p>{nimi}</p>
+          </Link>
+        </Card.Title>
+        <Link to={`/tuotteet/${id}`}>
+          <img src={kuva[0].kuva} alt="tuotteen kuva" />
+        </Link>
+        <Card.Body>
+          <Card.Subtitle>Artisaani: {tekijä}</Card.Subtitle>
+          <Card.Text>Hinta: {hinta}</Card.Text>
+          <Card.Text>Kategoria: {kategoria}</Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
