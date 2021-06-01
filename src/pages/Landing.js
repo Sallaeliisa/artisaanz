1dcb60f87f9bc5e9de9621d1f2d32c872cbf5312
Laura <laura.jarventie@edu.bc.fi>
2021-06-01 12:49:02 +0300

    example text to landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,6 +7,19 @@ const Landing = () => {
       <div className="herobanner-cont">
         <img className="herobanner" src={herobanner} alt="herobanner img" />
       </div>
+      <>
+        <h1>Tervetuloa</h1>
+        <p>
+          Tanskalaisilla oli "hygge" jo aikoja sitten, mutta myös me suomalaiset
+          olemme löytäneen kotoilun ja onhan meillä oma Strömsökin! Meistä on
+          kuoritunut mitä taidokkaampia käsitöiden tekijöitä, varsinaisia
+          artesaaneja. Tämä sivu on omistettu kaikille meille harrastelijoille,
+          ja miksei myös ammattilaisille. Tarkoituksena on löytää omat kodit
+          kaikille näille rakkaudella tehdyille käsitöille, koska määräänsä
+          enempää ei voi sukulaisille ja ystäville antaa samoja lahjoja
+          merkkipäivästä toiseen.
+        </p>
+      </>
     </main>
   );
 };
