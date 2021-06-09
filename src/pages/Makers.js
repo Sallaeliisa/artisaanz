a5b02353a6af41c21818486387f826a6fc4010b5
Akseli <s2100140@edu.bc.fi>
2021-06-09 11:34:41 +0300

    hardcoded kategoriat and link to pussukat in Artesaanit page

--- src/pages/Makers.js
+++ src/pages/Makers.js
@@ -2,112 +2,143 @@ import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import ProductCard from "../Components/ProductCard";
+import Spinner from "react-bootstrap/Spinner";
 
 const Makers = () => {
   const [maker, setMaker] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState("");
 
   useEffect(() => {
     axios
       .get("https://artisaanz.herokuapp.com/seller/all/")
-      .then(setLoading(true))
       .then((response) => setMaker(response.data));
-    setLoading(true);
+    setLoading(false);
   }, []);
 
+  if (maker.id === 3) {
+    setProducts("Tuotteet: kassit, pussukat, käsilaukut");
+  }
+  if (maker.id === 2) {
+    setProducts("Tuotteet: villasukat");
+  }
+  if (maker.id === 1) {
+    setProducts("Tuotteet: leivonnaiset");
+  }
+
   const makers = maker.map((maker) => {
-    return (
-      <Card className="makersCard" key={maker.id}>
-        <Card.Body>
-          <Card.Title>{maker.nimi}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {maker.tuotteet}
-          </Card.Subtitle>
-          <Card.Text>{maker.esittely}</Card.Text>
-          <Link
-            to={{
-              pathname: "/tuotteet",
-              state: { seller: maker.nimi },
-            }}
-          >
-            Artesaanin tuotteet
-          </Link>
-        </Card.Body>
-      </Card>
-    );
+    if (maker.id === 3) {
+      return (
+        <Card className="makersCard" key={maker.id}>
+          {loading === true && (
+            <Spinner
+              className="productSpinner"
+              animation="border"
+              variant="secondary"
+            />
+          )}
+          <Card.Body>
+            <Card.Title>{maker.nimi}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              Tuotteet: kassit, pussukat, käsilaukut
+            </Card.Subtitle>
+            <Card.Text>{maker.esittely}</Card.Text>
+            <Link
+              to={{
+                pathname: "/tuotteet",
+                state: { seller: maker.nimi },
+              }}
+            >
+              Artesaanin tuotteet
+            </Link>
+            <Card.Link href="https://pussukat.wordpress.com/" target="_blank">
+              Pussukat kotisivu
+            </Card.Link>
+          </Card.Body>
+        </Card>
+      );
+    } else if (maker.id === 2) {
+      return (
+        <Card className="makersCard" key={maker.id}>
+          {loading === true && (
+            <Spinner
+              className="productSpinner"
+              animation="border"
+              variant="secondary"
+            />
+          )}
+          <Card.Body>
+            <Card.Title>{maker.nimi}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              Tuotteet: villasukat
+            </Card.Subtitle>
+            <Card.Text>{maker.esittely}</Card.Text>
+            <Link
+              to={{
+                pathname: "/tuotteet",
+                state: { seller: maker.nimi },
+              }}
+            >
+              Artesaanin tuotteet
+            </Link>
+          </Card.Body>
+        </Card>
+      );
+    } else if (maker.id === 1) {
+      return (
+        <Card className="makersCard" key={maker.id}>
+          {loading === true && (
+            <Spinner
+              className="productSpinner"
+              animation="border"
+              variant="secondary"
+            />
+          )}
+          <Card.Body>
+            <Card.Title>{maker.nimi}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              Tuotteet: leivonnaiset
+            </Card.Subtitle>
+            <Card.Text>{maker.esittely}</Card.Text>
+            <Link
+              to={{
+                pathname: "/tuotteet",
+                state: { seller: maker.nimi },
+              }}
+            >
+              Artesaanin tuotteet
+            </Link>
+          </Card.Body>
+        </Card>
+      );
+    } else {
+      return (
+        <Card className="makersCard" key={maker.id}>
+          {loading === true && (
+            <Spinner
+              className="productSpinner"
+              animation="border"
+              variant="secondary"
+            />
+          )}
+          <Card.Body>
+            <Card.Title>{maker.nimi}</Card.Title>
+            <Card.Text>{maker.esittely}</Card.Text>
+            <Link
+              to={{
+                pathname: "/tuotteet",
+                state: { seller: maker.nimi },
+              }}
+            >
+              Artesaanin tuotteet
+            </Link>
+          </Card.Body>
+        </Card>
+      );
+    }
   });
 
-  return (
-    <main className="makers">
-      {makers}
-      {/* <Card className="makersCard">
-        <Card.Body>
-          <Card.Title>Riitta Järventie</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Tuotteet: kassit, pussukat, käsilaukut
-          </Card.Subtitle>
-          <Card.Text>
-            Olen aina pitänyt käsitöiden tekemisestä ja nyt eläkeläisenä minulla
-            on runsaasti aikaa tehdä erilaisia tuotteita. Ohessa olevasta
-            linkistä löydät lisää tuotoksiani.
-          </Card.Text>
-          <Link
-            to={{
-              pathname: "/tuotteet",
-              state: { seller: "Riitta Järventie" },
-            }}
-          >
-            Artesaanin tuotteet
-          </Link>
-          <Card.Link href="https://pussukat.wordpress.com/" target="_blank">
-            Pussukat kotisivu
-          </Card.Link>
-        </Card.Body>
-      </Card>
-
-      <Card className="makersCard">
-        <Card.Body>
-          <Card.Title>Akseli Miettinen</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Tuotteet: Leivonnaiset
-          </Card.Subtitle>
-          <Card.Text>
-            Lyyti hapanjuureni täytti juuri vuoden. Tästä ikiomasta juuresta
-            leivotut näyttävät ja maukkaat leivät ovat intohimoni. Kehittelen
-            jatkuvasti uusia reseptejä ja makuvivahteita.
-          </Card.Text>
-          <Link
-            to={{
-              pathname: "/tuotteet",
-              state: { seller: "Akseli Miettinen" },
-            }}
-          >
-            Artesaanin tuotteet
-          </Link>
-        </Card.Body>
-      </Card>
-
-      <Card className="makersCard">
-        <Card.Body>
-          <Card.Title>Salla Vuorikko</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Tuotteet: Villasukat
-          </Card.Subtitle>
-          <Card.Text>
-            Neulominen rentouttaa ja on mukavaa puuhaa. Sukkia on syntynyt sitä
-            tahtia että tutut ja sukulaiset ovat omansa jo saaneet. Nyt
-            lopuillekin sukkapareille toivottavasti löytyy tätä kautta oma koti.{" "}
-          </Card.Text>
-          <Link
-            to={{ pathname: "/tuotteet", state: { seller: "Salla Vuorikko" } }}
-          >
-            Artesaanin tuotteet
-          </Link>
-        </Card.Body>
-      </Card> */}
-    </main>
-  );
+  return <main className="makers">{makers}</main>;
 };
 
 export default Makers;
